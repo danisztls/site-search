e5bad8b9240dbcea088e507564659293acf37be0
Daniel Souza <me@posix.dev.br>
2021-10-09 01:41:41 -0300

    Bump to 3.2.1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @name     lite-search
  * @desc     A standalone instant search component.
  * @author   Daniel Souza <me@posix.dev.br>
- * @version  3.2.0
+ * @version  3.2.1
  * @license  MIT
  */
 
