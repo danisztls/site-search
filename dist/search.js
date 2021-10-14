ca7a7d4df74d81afe2cc7873209af31dc68f9177
Daniel Souza <me@posix.dev.br>
2021-10-14 16:28:35 -0300

    Build distributables for v3.4.2

--- dist/search.js
+++ dist/search.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25824bfd029c78ef829a333b1c8981a23c70169ef5e28e5610a01422b423ef72
-size 46495
+oid sha256:7e3b96410e9ca48880e6c930aef3175c2bc3d71f807ddc1a778a5856e35b003a
+size 46367
