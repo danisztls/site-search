f73c7a4751a3def67921f42c704b3beef7b52f0a
Daniel Souza <me@posix.dev.br>
2022-03-23 01:54:57 -0300

    Release v3.8.2

--- dist/search.js
+++ dist/search.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:131a5723be4dbe96562f1536e99bdfba3019b9af6cc224768fe5e373f36ba25b
-size 47497
+oid sha256:8aa238a758b15b7a963838c23195a11c2133c72e9c83b4a883f7eba3b74786a5
+size 47331
